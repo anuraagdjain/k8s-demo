60d0d296760deffcf2d1e067aa356a6e6b82fd9d
Anuraag <thehray@gmail.com>
2021-02-07 21:54:29 +0000

    fix: typo ofr deployment

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 # push docker images to repository
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=stephengrider/multi-server:latest
-kubectl set image deployments/client-deployment server=stephengrider/multi-client:latest
-kubectl set image deployments/worker-deployment server=stephengrider/multi-worker:latest
+kubectl set image deployments/client-deployment client=stephengrider/multi-client:latest
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:latest
